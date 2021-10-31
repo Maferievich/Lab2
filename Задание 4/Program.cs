69e1458e21b324649c168203df870adfaf41a27e
maf fik <tufurfil@mail.ru>
2021-11-01 00:31:08 +0300

    lqwe

---
+++ Задание 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  q;
+            double x, slg, precos,b,c,fact;
+            x = double.Parse(Console.ReadLine());
+            q = int.Parse(Console.ReadLine());
+            precos = 1;
+            slg = q+1;
+            b = 2;
+            c = 2;
+
+
+            for (int t=1;slg>q;t++)
+            {
+                fact = b;
+                for (int i = 1; i < b; i++)
+                {
+                    fact *= i;
+                }
+                b += 2;
+                slg = (Math.Pow(x, c))/fact;
+                c += 2;
+                if (t%2==0)
+                {
+                    precos += slg;
+                }
+                else
+                {
+                    precos -= slg;
+                }
+                Console.WriteLine(t);
+            }
+            Console.WriteLine("cos(x)= " + precos);
+        }
+    }
+}
---
+++ Задание 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
