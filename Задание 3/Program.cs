18cb2aaf1948c6deb769124d214658977e71d7dc
maf fik <tufurfil@mail.ru>
2021-11-01 00:31:08 +0300

    lqwe

---
+++ Задание 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,d;
+            a = 0;
+            b = 1;
+            c = 0;
+            d = 0;
+            while (c<10000)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+                if ((c > 999)&(c<10000))
+                    d++;
+            }
+            Console.WriteLine("4-х значyных числел в ряде Фибоначчи = " + d);
+        }
+    }
+}
