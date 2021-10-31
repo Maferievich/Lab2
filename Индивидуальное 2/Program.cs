2bc7c95524020512c281a557be24968bd41ded22
maf fik <tufurfil@mail.ru>
2021-11-01 01:58:17 +0300

    lol

---
+++ Индивидуальное 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Индивидуальное_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for(int i=1; i <=10000; i++)
+            {
+                if(10000%i==0)
+                {
+                    a = 10000 / i;
+                    if (a % 2 == 1)
+                        Console.WriteLine(a);
+
+                }
+            }
+        }
+    }
+}
