ba2b7437bcee7a5ee2c68958028890803e2edfba
maf fik <tufurfil@mail.ru>
2021-10-31 22:41:46 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double a, b, c, d , x1, x2;
+            Console.WriteLine("Введите значения a,b,c для уравнения ax^2+bx+c=0");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            d = Math.Pow(b, 2) - 4 * a * c;
+            if (d > 0)
+            {
+                d = Math.Sqrt(d);
+                x1 = (-b + d) / (2 * a);
+                x2 = (-b - d) / (2 * a);
+                Console.WriteLine("Корни уравнения = " + x1, +x2);
+            }
+            if (d == 0)
+            {
+                x1 = (-b) / (2 * a);
+                Console.WriteLine("Корень уравнения = " + x1);
+            }
+            if (d < 0)
+
+        }
+    }
+}
---
+++ задание 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, b,t;
+            int a;
+            Console.Write("Введите кол-во: ");
+            a = int.Parse(Console.ReadLine());
+            pi = 4;
+            b = 3;
+            t = 1;
+            if (a <= 1)
+            {
+                Console.WriteLine("П = " + pi);
+            }
+            else
+            {
+                for (int c = 1; c <= a; c++) 
+                {
+                    if ((c % 2) == 0)
+                    {
+                        t = t + (1 / b);
+                        b = b + 2;
+                    }
+                    else
+                    {
+                        t = t - (1 / b);
+                        b = b + 2;
+                    }
+                }
+                pi = t * 4;
+                Console.WriteLine("П = " + pi);
+ 
+            }
+        }
+    }
+}
