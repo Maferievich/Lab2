a43b57e4c7a0d1c74e368a7725559dd55826f46b
maf fik <tufurfil@mail.ru>
2021-11-01 11:05:39 +0300

    изменено

--- Задание 6/Program.cs
+++ Задание 6/Program.cs
@@ -9,33 +9,34 @@ namespace Задание_6
             Console.Write("Введие число от 0 до 100: ");
             int a;
             a = int.Parse(Console.ReadLine());
-            if ((a >= 0)&(a<=100))
+            for (int i = 0; i <= 100; i++)
             {
-                if ((a >= 0) & (a < 15))
+                if ((i >= 0) & (i <= 100))
                 {
-                    if (a % 100 == 1)
-                        Console.WriteLine(a + " год");
-                    
-                    else
-                        Console.WriteLine(a + " лет");
-                }
+                    if ((i >= 5) & (i < 15))
+                    {
+                        if (i % 100 == 1)
+                            Console.WriteLine(i + " год");
+
+                        else
+                            Console.WriteLine(i + " лет");
+                    }
 
 
-                    if (a >= 15)
+                    if ((i >= 15) | (i < 5))
                     {
 
 
-                        if (a % 10 == 1)
-                            Console.WriteLine(a + " год");
-                        if ((a % 10 > 1) & (a % 10 < 5))
-                            Console.WriteLine(a + " года");
-                        if ((a%10==0)&(a%10>=5))
-                            Console.WriteLine(a + " лет");
+                        if (i % 10 == 1)
+                            Console.WriteLine(i + " год");
+                        if ((i % 10 > 1) & (i % 10 < 5))
+                            Console.WriteLine(i + " года");
+                        if ((i % 10 == 0) & (i % 10 >= 5))
+                            Console.WriteLine(i + " лет");
                     }
-                
+
+                }
             }
-            else
-                Console.WriteLine("от 0 до 100, никак иначе");
         }
     }
 }
